5489fc17a3e585ed0c5072447ed4923c524fd752
James <jhchadwick@protonmail.com>
2020-02-23 03:02:00 +0000

    more examples, need to update docs

--- SDJP/sdjp.c
+++ SDJP/sdjp.c
@@ -65,6 +65,7 @@ char* strndup(char* str, size_t chars)
 	return buffer;
 }
 
+#pragma region Basic Get First Function
 /*
 	basic_get_first is a function that will basically pull the value from a defined name
 	in other words if you know your response from your server will be:
@@ -128,52 +129,148 @@ char* basic_get_first(char* json_string, char* name)
 		// these are integers we define as part of the sdjptok_t object
 		// .start = the position in our JSON string that is the first character of the tokenized object
 		// .end = the position in our JSON string that is the last character of the tokenized object
+		// we can now get the size of the token
 		int token_size = token[2].end - token[2].start;
 
+		// not that we have the size we can use our string duplication function (strndup) to create a new string
+		// containing only the data contained within that token, token[2] = value so we get a string containing just the value
 		char* result = strndup(json_string + token[2].start, token_size);
+
+		// and then we return it
 		return result;
 	}
+
+	// if you hit this the compare failed so we just return NULL
+	// feel free to add some sort of error code system (makes life easier in the long run)
 	else
 	{
 		return NULL;
 	}
 
 }
+#pragma endregion
+
+
+// First we want to return the corresponding value of some of these fields, to do this we are going to create a struct called WhoAmI
+// this contains entries for each value we want to retrieve, not sure if you guys have come across structs yet, if not a good guide:
+// https://www.geeksforgeeks.org/structures-c/ which covers the basics
+typedef struct
+{
+	char* FirstName;
+	char* MiddleName;
+	char* LastName;
+	char* Occupation;
+	int ErrorCode;
+} WhoAmI;
+
+WhoAmI basic_nested(char* json_string)
+{
+	// First we create our empty struct we are going to use to hold the values
+	WhoAmI whoami;
+
+	// We create our parser object
+	sdjp_parser parser;
+
+	// We create the number of tokens we want (128 seems like a solid number)
+	// TODO: maybe have this passed as a function parameter
+	sdjptok_t token[128];
+
+	// Initalizes our parser object
+	sdjp_init(&parser);
+
+	// So this is similar to the basic_get_first version
+	// For number of tokens I use sizeof(token) / sizeof(token[0])
+	// This just evaluates to 2048/16 = 128
+	// it saves us having to update the code in two places if the max number of tokens changes
+	int result = sdjp_parse(&parser, json_string, strlen(json_string), token, sizeof(token) / sizeof(token[0]));
+
+	// If result is less than 0 an error code was returned, we print it
+	if (result < 0)
+	{
+		printf("Failed to tokenize JSON: %d\n", result);
+		// Set our error code
+		whoami.ErrorCode = 1;
+		// Return from function
+		return whoami;
+	}
+
+	// This checks if the first entry in the token array is the expected type SDJP_OBJECT, if not then something has likely gone wrong with our tokenizer
+	if (result == 0 || token[0].type != SDJP_OBJECT)
+	{
+		printf("first entry in token array not a SDJP_OBJECT, something went wrong");
+		// Set our error code
+		whoami.ErrorCode = 1;
+		// Return from function
+		return whoami;
+	}
+
+	// Now we get into our loop, the logic is similar to our first implementation except this time we check for a bunch of predefined names
+	// If we find one then we append it to the appropriate field inside our WhoAmI structure
+	for (int i = 1; i < result; i++)
+	{
+		if (json_compare(json_string, &token[i], "FirstName") == 0)
+		{
+			// copies our string using the values provided by our token
+			// we use i+1 as i is the token for our name and +1 from that is the associated value
+			whoami.FirstName = strndup(json_string + token[i+1].start, token[i+1].end - token[i+1].start);
+			i++;
+		}
+
+		else if (json_compare(json_string, &token[i], "MiddleName") == 0)
+		{
+			whoami.MiddleName = strndup(json_string + token[i + 1].start, token[i + 1].end - token[i + 1].start);
+			i++;
+		}
+
+		else if (json_compare(json_string, &token[i], "LastName") == 0)
+		{
+			whoami.LastName = strndup(json_string + token[i + 1].start, token[i + 1].end - token[i + 1].start);
+			i++;
+		}
+
+		else if (json_compare(json_string, &token[i], "Occupation") == 0)
+		{
+			whoami.Occupation = strndup(json_string + token[i + 1].start, token[i + 1].end - token[i + 1].start);
+			i++;
+		}
+	}
+
+	// if we get here then something is being returned so just set the error code to 0 (success)
+	whoami.ErrorCode = 0;
+
+	// return
+	return whoami;
+
+}
+
 
 
 int main()
-{
-
-
-
+{	
+	// Calls our basic implementation where we know the name of our JSON field beforehand (in this case its Hello)
+	// see function for more details
 	char* json_test = "{\"Hello\":\"World\"}";
-
 	char* bgf_result = basic_get_first(json_test, "Hello");
 
-	sdjp_parser p;
 
-	sdjptok_t t[128];
+	// A bit more oomfy example, here we have a raw JSON object with multiple root fields
+	// What we are going to do is loop through each root field and check if the name is something we expect
+	// if it is then we are going to process it. Again with this example we assume we know the name ahead of time.
 
-	sdjp_init(&p);
-
-	int r = sdjp_parse(&p, json_test, strlen(json_test), t, 128);
-
-
-	if (json_compare(json_test, &t[1], "Hello") == 0) 
-	{
-		/* We may use strndup() to fetch string value */
-		printf("- Hello: %.*s\n", t[1 + 1].end - t[1 + 1].start,
-			json_test + t[1 + 1].start);
-
-
-		int token_size = t[2].end - t[2].start;
-
-		char* result = strndup(json_test + t[2].start, token_size);
-		printf("Result is: %s",result);
-
-	}
-
+	// Here is our example JSON, we have 4 root fields here FirstName, MiddleName, LastName and Occupation
+	char* json_test2 = "{\"FirstName\":\"James\", \"MiddleName\":\"Henry\", \"LastName\":\"Chadwick\", \"Occupation\":\"Pro Nerd\"}";
 	
+	// call our function
+	WhoAmI test2 = basic_nested(json_test2);
+
+	// print the results
+	printf("FirstName = %s\n", test2.FirstName);
+	printf("MiddleName = %s\n", test2.MiddleName);
+	printf("LastName = %s\n", test2.LastName);
+	printf("Occupation = %s\n", test2.Occupation);
+
+	// TODO for fun try changing a field in our json string to an unexpected one, what happens?
+	// How could you solve that problem? HINT check the test2 struct field you removed before you try to print it
 
 	return 0;
 }
